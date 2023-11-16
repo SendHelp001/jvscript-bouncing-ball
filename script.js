f7e93c1bef9e6842c232bb3259fcde48d0cd014c
irvento <129978135+irvento@users.noreply.github.com>
2023-11-16 11:31:11 +0800

    final bounce

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
+// Define a variable to store the game piece
 var myGamePiece;
 
+// Function to start the game
 function startGame() {
-    myGamePiece = new component(30, 30, "red", 80, 75, "circle");
-    myGameArea.start();
+    // Create a new game piece with specific properties (width, height, color, position, and type)
+    myGamePiece = new component(30, 30, "red", 80, 75, "circle");                 // Ball object initialization
+    myGameArea.start();                                                          // Start the game area (canvas) with gravity
 }
 
+// Object representing the game area (canvas) and its properties
 var myGameArea = {
-    canvas: document.createElement("canvas"),
+    canvas: document.createElement("canvas"),                                   // Create a new canvas element
     start: function () {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
+        this.canvas.width = 1580;                                               // Set the width of the canvas
+        this.canvas.height = 270;                                               // Set the height of the canvas
         this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 20);
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);   // Insert the canvas into the document body
+        this.interval = setInterval(updateGameArea, 20);                        // Set up a timer to update the game area
     },
     stop: function () {
-        clearInterval(this.interval);
+        clearInterval(this.interval);                                           // Stop the game interval
     },
     clear: function () {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);    // Clear the canvas
     },
 };
 
+// Constructor function to create game components (e.g., the ball)
 function component(width, height, color, x, y, type) {
     this.type = type;
     this.width = width;
@@ -61,8 +66,41 @@ function component(width, height, color, x, y, type) {
     };
 }
 
+// Function to update the game area (clear, update position, and draw the game piece)
 function updateGameArea() {
     myGameArea.clear();
     myGamePiece.newPos();
     myGamePiece.update();
 }
+
+// Event listener to handle key presses for WASD controls
+window.addEventListener('keydown', function (e) {
+    switch (e.key) {
+        case 'a':
+            myGamePiece.speedX = -5;  // Move left
+            break;
+        case 'd':
+            myGamePiece.speedX = 5;   // Move right
+            break;
+        case 'w':
+            myGamePiece.speedY = -3;  // Move up
+            break;
+        case 's':
+            myGamePiece.speedY = 1;   // Move down
+            break;
+    }
+});
+
+// Event listener to handle key releases and stop the movement
+window.addEventListener('keyup', function (e) {
+    switch (e.key) {
+        case 'a':
+        case 'd':
+            myGamePiece.speedX = 0;   // Stop horizontal movement
+            break;
+        case 'w':
+        case 's':
+            myGamePiece.speedY = 0;   // Stop vertical movement
+            break;
+    }
+});
